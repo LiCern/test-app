625ceb0da9ba51f38c4140c693882fd328e3a06a
LiCern <lisacernilogar@me.com>
2020-09-24 15:23:28 +0100

    React app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import getData from "./utils/data_helpers";
 
 function App() {
+  const [userData, setUserData] = React.useState(null);
+
+  React.useEffect(() => {
+    const path = "https://week7-chjm.herokuapp.com/harvest";
+    getData(path).then((data) => setUserData(data));
+  }, []);
+  console.log(userData);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <b>Test App</b>
       </header>
+      {/* const foodArray = userData.map((food) => {return <p>food</p>}); */}
+      {userData ? (
+        userData.map((food) => <p key={food.id}>{food.food_type}</p>)
+      ) : (
+        <p>Loading..</p>
+      )}
+      {/* )) ? foodArray : <p>loading...!!!</p>} */}
     </div>
   );
 }
