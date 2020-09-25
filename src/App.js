ca398f25e44e9ab38f9576b260d5cd3f36b52344
LiCern <lisacernilogar@me.com>
2020-09-25 09:25:54 +0100

    More errors

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import "./App.css";
 import getData from "./utils/data_helpers";
 
 function App() {
-  let path = "";
-  if (process.env.NODE_ENV === "development") {
-    path = "http://localhost:4000/harvest";
-  }
+  // let path = "";
+  // if (process.env.NODE_ENV === "development") {
+  //   path = "http://localhost:4000/harvest";
+  // }
 
-  if (process.env.NODE_ENV === "production") {
-    path = "https://week7-chjm.herokuapp.com/harvest";
-  }
+  // if (process.env.NODE_ENV === "production") {
+  //   path = "https://week7-chjm.herokuapp.com/harvest";
+  // }
 
   const [userData, setUserData] = React.useState(null);
   console.log(process.env.NODE_ENV);
   console.log(process.env.REACT_APP_BACKEND_URL);
   React.useEffect(() => {
-    // const path = process.env.REACT_APP_BACKEND_URL; // process.env accesses all env files
+    const path = process.env.REACT_APP_BACKEND_URL; // process.env accesses all env files
     getData(path).then((data) => setUserData(data));
   }, []);
   console.log(userData);
